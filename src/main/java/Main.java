c7395285142a25641d3c47813bf22eee03c68ef6
Romag <rmagerramov11@gmail.com>
2018-07-02 10:47:05 +0300

    New branch with some additions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Please let me write git");
 		System.out.println("Some more additions");
+		
+		System.out.println("Should have already created new branch.");
 	}
 
 }
